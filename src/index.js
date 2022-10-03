57ad4ed0d9e43b311e27f90ee88922c934109899
oiraH1412 <iqyuhario@gmail.com>
2022-10-03 15:17:17 +0700

    Co-authored-by: TheAZF <TheAZF@users.noreply.github.com>

---
+++ src/index.js
@@ -0,0 +1,28 @@
+function gambarBox(container, row, col, huruf=" ") {
+    const box = document.createElement("div");
+    box.namaClass = "box";
+    box.id = `box${row}${col}`;
+    box.konten = huruf;
+    
+    container.appendChild(box);
+    return box;
+}
+
+function gambarGrid(container) {
+    const grid = document.createElement("grid");
+    grid.namaClass = "grid"
+    
+    for (let i = 0; i <6;i++){
+        for(let j = 0; j <5; j++){ 
+            gambarBox(grid, i, j);
+        }
+    }
+    container.appendChild(grid);
+}
+
+function mulai() {
+    const game = document.getElementById("game");
+    gambarGrid(game);
+}
+
+mulai();
