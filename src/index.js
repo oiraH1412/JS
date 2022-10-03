81dabff6afda018b249875cad9ad00f34afc10b1
oiraH1412 <iqyuhario@gmail.com>
2022-10-03 15:45:00 +0700

    Co-authored-by: TheAZF <TheAZF@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
-function gambarBox(container, row, col, huruf=" ") {
+function gambarBox(container, row, col, huruf = " ") {
     const box = document.createElement("div");
-    box.namaClass = "box";
+    box.className = "box";
     box.id = `box${row}${col}`;
     box.konten = huruf;
-    
+
     container.appendChild(box);
     return box;
 }
 
 function gambarGrid(container) {
     const grid = document.createElement("grid");
-    grid.namaClass = "grid"
-    
-    for (let i = 0; i <6;i++){
-        for(let j = 0; j <5; j++){ 
+    grid.className = "grid"
+
+    for (let i = 0; i < 6; i++) {
+        for (let j = 0; j < 5; j++) {
             gambarBox(grid, i, j);
         }
     }
